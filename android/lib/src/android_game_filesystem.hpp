ad823e247e971fdc377f0a945cd0e1c3bf099eb3
Julien Jorge <julien.jorge@stuff-o-matic.com>
2013-09-01 23:16:04 +0200

    Add a subclass of default_game_filesystem to write the configuration files in the application's internal directory.

---
+++ android/lib/src/android_game_filesystem.hpp
@@ -0,0 +1,43 @@
+/*
+  Copyright (C) 2012 Stuffomatic Ltd. <contact@stuff-o-matic.com>
+
+  All rights reserved.
+
+  See the accompanying license file for details about usage, modification and
+  distribution of this file.
+*/
+/**
+ * \file
+ * \brief The android_game_filesystem class is an implementation of the
+ *        base_game_filesystem class that allow to write game files for an
+ *        Android application.
+ * \author Julien Jorge
+ */
+#ifndef __RP_ANDROID_GAME_FILESYSTEM_HPP__
+#define __RP_ANDROID_GAME_FILESYSTEM_HPP__
+
+#include "engine/system/default_game_filesystem.hpp"
+
+namespace rp
+{
+  /**
+   * \brief The android_game_filesystem class is an implementation of the
+   *        base_game_filesystem class that allow to write game files for an
+   *        Android application.
+   * \author Julien Jorge
+   */
+  class android_game_filesystem:
+    public bear::engine::default_game_filesystem
+  {
+  public:
+    android_game_filesystem( std::string game_name );
+
+    virtual android_game_filesystem* clone() const;
+
+  private:
+    std::string get_android_root() const;
+    
+  }; // class android_game_filesystem
+} // namespace rp
+
+#endif // __RP_ANDROID_GAME_FILESYSTEM_HPP__
