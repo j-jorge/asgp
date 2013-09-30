2c951289dc329d8755bd8b0806acb26b5e0875e0
Julien Jorge <julien.jorge@stuff-o-matic.com>
2013-09-30 10:58:07 +0200

    Tells to keep the screen on while the game is running.

--- android/java/src/com/stuffomatic/asgp/ASGP.java
+++ android/java/src/com/stuffomatic/asgp/ASGP.java
@@ -1,11 +1,20 @@
 package com.stuffomatic.asgp;
 
 import org.libsdl.app.SDLActivity;
+import android.view.WindowManager;
+import android.os.Bundle;
 
 public class ASGP extends SDLActivity
 {
     static {
         System.loadLibrary( "gnustl_shared" );
         System.loadLibrary( "andy-super-great-park" );
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
     }
 }
