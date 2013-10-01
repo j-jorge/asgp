5a2b7c594dd679b0e6da472fb625101f4caa8f9e
Julien Jorge <julien.jorge@stuff-o-matic.com>
2013-10-01 17:25:14 +0200

    Hides the navigation bar on Android.

--- android/java/src/com/stuffomatic/asgp/ASGP.java
+++ android/java/src/com/stuffomatic/asgp/ASGP.java
@@ -1,6 +1,15 @@
+/*
+  Copyright (C) 2012 Stuffomatic Ltd. <contact@stuff-o-matic.com>
+
+  All rights reserved.
+
+  See the accompanying license file for details about usage, modification and
+  distribution of this file.
+*/
 package com.stuffomatic.asgp;
 
 import org.libsdl.app.SDLActivity;
+import android.view.View;
 import android.view.WindowManager;
 import android.os.Bundle;
 
@@ -16,5 +25,7 @@ public class ASGP extends SDLActivity
         super.onCreate(savedInstanceState);
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        getWindow().getDecorView().setSystemUiVisibility
+            ( View.SYSTEM_UI_FLAG_LOW_PROFILE );
     }
 }
