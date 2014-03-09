cc95fa4a680918861fa2b6e4136a9cbd208f9742
Julien Jorge <julien.jorge@stuff-o-matic.com>
2014-03-09 19:08:21 +0100

    Use the immersive mode on Android.

--- android/java/src/com/stuffomatic/asgp/ASGP.java
+++ android/java/src/com/stuffomatic/asgp/ASGP.java
@@ -43,7 +43,12 @@ public class ASGP extends SDLActivity
  
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         getWindow().getDecorView().setSystemUiVisibility
-            ( View.SYSTEM_UI_FLAG_LOW_PROFILE );
+            ( View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+              | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+              | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+              | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+              | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+              | View.SYSTEM_UI_FLAG_LOW_PROFILE );
     } // onCreate()
 
     /**
