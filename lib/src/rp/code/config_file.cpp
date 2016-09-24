dbdbdd2aa47b2334098bafdf4223b4d7c43a954b
Julien Jorge <julien.jorge@stuff-o-matic.com>
2016-09-24 22:21:55 +0200

    Turn off the music by default on Android.

--- lib/src/rp/code/config_file.cpp
+++ lib/src/rp/code/config_file.cpp
@@ -27,7 +27,13 @@ rp::config_file::config_file()
   : m_fullscreen(false),
     m_dumb_rendering(true),
     m_sound_on(true),
-    m_sound_volume(1), m_music_on(true), m_music_volume(0.7),
+    m_sound_volume(1),
+#ifdef __ANDROID__
+    m_music_on(false),
+#else
+    m_music_on(true),
+#endif
+    m_music_volume(0.7),
     m_config_name("config")
 {
   const bear::engine::game& g( bear::engine::game::get_instance() );
