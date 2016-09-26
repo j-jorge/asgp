29b299dfecbd965b1c7f0eb2ade5e01573c011ba
Julien Jorge <julien.jorge@stuff-o-matic.com>
2016-09-26 22:46:36 +0200

    Align the release apk.

--- build-scripts/android/60-apk.sh
+++ build-scripts/android/60-apk.sh
@@ -24,11 +24,23 @@ TARGET_APK="$PWD/build/outputs/apk/$APP_NAME-$BUILD_MODE.apk"
 if [ "$BUILD_MODE" = "release" ]
 then
     gradle assembleRelease
-    GENERATED_APK="$PWD/build/outputs/apk/java-release-unsigned.apk"
 
+    GENERATED_APK="$PWD/build/outputs/apk/java-release-unsigned.apk"
     jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 \
               -keystore ~/.keytool/release.keystore \
               "$GENERATED_APK" play-store
+
+    UNALIGNED_APK="$PWD/build/outputs/apk/java-release-unaligned.apk"
+    mv "$GENERATED_APK" "$UNALIGNED_APK"
+
+    BUILD_TOOLS_VERSION=$(grep buildToolsVersion build.gradle | cut -d'"' -f2)
+    
+    ALIGNED_APK="$PWD/build/outputs/apk/java-release-aligned.apk"
+    
+    $ANDROID_SDK/build-tools/$BUILD_TOOLS_VERSION/zipalign -v -f 4 \
+        "$UNALIGNED_APK" "$ALIGNED_APK"
+
+    GENERATED_APK="$ALIGNED_APK"
 else
     gradle assembleDebug
 
